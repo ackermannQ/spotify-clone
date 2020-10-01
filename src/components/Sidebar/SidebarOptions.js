94478bf6a850e81d25f4d1d0ca1ae62a190c8568
ackermannQ <ackermann.quentin@gmail.com>
2020-10-01 18:04:49 +0200

    * Adding a SidebarOptions component

---
+++ src/components/Sidebar/SidebarOptions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SidebarOptions.css"
+
+function SidebarOptions ({ title, Icon }) {
+  return (
+    <div className="sidebarOptions">
+        
+    </div>
+  );
+}
+
+export default SidebarOptions;
